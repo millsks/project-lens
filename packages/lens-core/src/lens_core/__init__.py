d076b177f345473da57612ea2d2128acaabae8a9
Kevin Mills <millsks@gmail.com>
2026-01-01 16:46:41 -0600

    chore: remove lens-api and lens-core packages as part of project restructuring

--- packages/lens-core/src/lens_core/__init__.py
+++
@@ -1,8 +0,0 @@
-"""LENS Core - Core functionality for Lineage & Enterprise eXplainer Service."""
-
-try:
-    from lens_core._version import version as __version__
-except ImportError:
-    __version__ = "0.0.0+unknown"
-
-__all__ = ["__version__"]
